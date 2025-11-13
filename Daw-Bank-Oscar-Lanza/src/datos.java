2562d26ae35bc13d83d2dfd015fd6bea0edffa03
COMPU-S033\daw1 <camiloarone@gmail.com>
2025-11-13 09:49:55 +0100

    no message

--- Daw-Bank-Oscar-Lanza/src/datos.java
+++ Daw-Bank-Oscar-Lanza/src/datos.java
@@ -2,7 +2,32 @@ public class datos {
 
     private String iban;
     private String titular;
-    private String saldo;
-}
+    private double saldo;
 
 //Constructores
+public datos (String iban, String titular, double saldo){
+    this.iban = iban;
+    this.titular = titular;
+    this.saldo = 0;
+}
+//Getters y setter
+
+
+    public String getIban() {
+        return iban;
+    }
+    public String getTitular() {
+    return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+    public double getSaldo() {
+    return saldo;
+    }
+    public void setSaldo(double saldo) {
+    this.saldo = saldo;
+    }
+
+}
--- Daw-Bank-Oscar-Lanza/src/movimientos.java
+++ Daw-Bank-Oscar-Lanza/src/movimientos.java
@@ -1,6 +1,40 @@
 public class movimientos {
 
-    private String iban;
-    private String retirada;
+    private double ingreso;
+    private double retirada;
     private String IDmovimiento;
+    private String fecha;
+
+
+    //Constructores
+    public movimientos(double ingreso, double retirada, String IDmovimiento, String fecha) {
+        this.ingreso = ingreso;
+        this.retirada = retirada;
+        this.IDmovimiento = IDmovimiento;
+        this.fecha = fecha;
+    }
+
+    //Getters-Setters
+
+    public double getingreso() {
+        return ingreso;
+    }
+    public void setingreso(double ingreso) {
+        this.ingreso = ingreso;
+    }
+    public double getretirada() {
+        return retirada;
+    }
+    public void setretirada(double retirada) {
+        this.retirada = retirada;
+    }
+    public String getIDmovimiento() {
+        return IDmovimiento;
+    }
+    public String getfecha() {
+        return fecha;
+    }
+
+
+
 }
