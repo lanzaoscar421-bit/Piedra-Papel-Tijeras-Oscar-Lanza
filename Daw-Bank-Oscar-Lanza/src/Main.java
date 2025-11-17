8226cacc3f3ff5f769c0f3758e9d417433fe2cad
COMPU-S033\daw1 <camiloarone@gmail.com>
2025-11-17 09:50:02 +0100

    no message

--- Daw-Bank-Oscar-Lanza/src/Main.java
+++ Daw-Bank-Oscar-Lanza/src/Main.java
@@ -1,35 +1,67 @@
 import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
+
 
         Scanner sc = new Scanner(System.in);
 
 
-        datos oscar = new datos(" ES9121000418450200051332", "Oscar-Lanza", 67.23);
-        System.out.println("Titular: " + oscar.getTitular());
-        System.out.println("Iban:" +  oscar.getIban());
-        System.out.println("Saldo:" + oscar.getSaldo());
+//        datos oscar = new datos(" ES9121000418450200051332", "Oscar-Lanza", 67.23);
+//        System.out.println("Titular: " + oscar.getTitular());
+//        System.out.println("Iban:" +  oscar.getIban());
+//        System.out.println("Saldo:" + oscar.getSaldo());
+//
+//
+//        movimientos Oscar_Movimientos = new movimientos(67, 67, "709823409A", "27 Julio 2025");
+//        System.out.println("ingreso:" + Oscar_Movimientos.getingreso());
+//        System.out.println("retirada:" + Oscar_Movimientos.getretirada());
+//        System.out.println("IDmovimiento:" + Oscar_Movimientos.getIDmovimiento());
+//        System.out.println("fecha:" + Oscar_Movimientos.getfecha());
 
 
-        movimientos Oscar_Movimientos = new movimientos(67, 67, "709823409A", "27 Julio 2025");
-        System.out.println("ingreso:" + Oscar_Movimientos.getingreso());
-        System.out.println("retirada:" + Oscar_Movimientos.getretirada());
-        System.out.println("IDmovimiento:" + Oscar_Movimientos.getIDmovimiento());
-        System.out.println("fecha:" + Oscar_Movimientos.getfecha());
+
 
 
 
         //Primero pondremos para que el usuario ponga sus datos
 
+        System.out.println("*******************************");
+        System.out.println("Bienvenido a tu cuenta de banco");
+        System.out.println("*******************************");
+
+        datos miCuenta = new datos(sc.nextLine(), sc.nextLine(), sc.nextDouble());
+
+        miCuenta.getIban();
+
+        int opcion = 0;
+        while (opcion != 6){
+
+            System.out.println("Pulse 1 para crear una cuenta: ");
+
+            System.out.println("Pulse 2 para ver la informacion de la cuneta ya creada:");
+
+            System.out.println("Pulse 3 para ingresar dinero:");
+
+            System.out.println("Pulse 4 para retirar dinero:");
+
+            System.out.println("Pulse 5 para mostrar los movimientos realizados en esta cuenta:");
+
+            System.out.println("Pulse 6 para salir del programa");
+
+            opcion = sc.nextInt();
 
 
-        String titular = "", iban = "", saldo = "";
-        System.out.println("Pon el nombre de la persona titular: ");
-        titular = sc.nextLine();
-        System.out.println("Pon el tu iban: ");
-        iban = sc.nextLine();
-        System.out.println("Inserte el saldo:");
-        saldo = sc.nextLine();
+            switch (opcion) {
+                case 1:
+
+                break;
+            }
+
+
+        }
+
+
 
 
 
--- Daw-Bank-Oscar-Lanza/src/datos.java
+++ Daw-Bank-Oscar-Lanza/src/datos.java
@@ -1,15 +1,18 @@
 public class datos {
 
     private String iban;
+    private String nombre;
+    private String apellidos;
+    private String direccion;
     private String titular;
     private double saldo;
 
-//Constructores
-public datos (String iban, String titular, double saldo){
-    this.iban = iban;
-    this.titular = titular;
-    this.saldo = saldo;
-}
+    //Constructores
+    public datos(String iban, String titular, double saldo) {
+        this.iban = iban;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
 //Getters y setter
 
 
@@ -17,17 +20,62 @@ public datos (String iban, String titular, double saldo){
         return iban;
     }
 
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getNombre() {
+
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+
+    }
     public String getTitular() {
-    return titular;
+        return titular;
     }
+
     public void setTitular(String titular) {
-        this.titular = titular;
+        this.titular = nombre+ "" + apellidos;
     }
+
     public double getSaldo() {
-    return saldo;
+        return this.saldo;
     }
     public void setSaldo(double saldo) {
-    this.saldo = saldo;
+
+        if(saldo>0){
+            System.out.println("Tienes dinero bro lo juro");
+            this.saldo =saldo;
+        }
+        else{
+            if(this.saldo<0){
+                this.saldo = 0;
+                System.out.println("Manin no tienes ni un puto duro");
+
+            }
+        }
+
+
     }
+
+
+
+
+    //Mostrar informacion de la cuenta
+
+
+
+
+
+
 
 }
