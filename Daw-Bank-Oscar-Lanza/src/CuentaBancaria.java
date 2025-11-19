f87243d75fc27bd90a58970175d483521f296974
DESKTOP-KS7Q6HV\lanza <lanzaoscar421@gmail.com>
2025-11-19 20:59:00 +0100

    no message

--- Daw-Bank-Oscar-Lanza/src/CuentaBancaria.java
+++ Daw-Bank-Oscar-Lanza/src/CuentaBancaria.java
@@ -5,52 +5,44 @@ public class CuentaBancaria {
     private double saldo;
 
     //Constructores
-    public CuentaBancaria(String iban, String titular, double saldo) {
-        this.iban = iban;
-        this.titular = titular;
-        this.saldo = 0;
+    public CuentaBancaria(String iban, String titular) {
+        this.iban = iban; // El usuario puede meter el Iban que quiera
+        this.titular = titular;//El usuario puede meter el Titular que quiera
+        this.saldo = 0;// Al ser siempre 0 no pongo arriba, double Saldo
     }
-//Getters y setter
+    //Getters y setter
+    //Getter para que se pueda acceder desde otra clase como main
+    //Setter para modificar el valor
+    //Y en las anteriores no pongo setters por el motivo de que no tiene sentido cambiar el valor
 
 
     public String getIban() {
         return iban;
     }
 
-    public void setIban(String iban) {
-        this.iban = iban;
-    }
     public String getTitular() {
         return titular;
-    }
-    public void setTitular(String titular) {
-        this.titular = titular;
     }
 
     public double getSaldo() {
         return this.saldo;
     }
-    public void setSaldo(double saldo) {
-
-        if(saldo>0){
-            System.out.println("Tienes dinero bro lo juro");
-            this.saldo =saldo;
-        }
-        else{
-            if(this.saldo<0){
-                this.saldo = 0;
-                System.out.println("Manin no tienes ni un puto duro");
-
-            }
-        }
+    public void setSaldo(){
 
     }
 
-
-
-
-
     //Mostrar informacion de la cuenta
+    //Ahora pondremos public ya que queremos que se vea la informacion en las otras clases
+
+    public String InformacionCuenta(){ //Esto serviria para poner toda la informacion que el usuario ah metido desde el main
+        String Info = "";
+        Info+="Iban: " + this.iban + "\n";//Ponemos this para que  pille el valor original
+        Info+="Titular: " + this.titular + "\n";
+        Info+="Saldo: " + this.saldo + "\n";
+        return Info; //Ponemos return Info ya que devuelve la informacion Info
+
+    }
+
 
 
 
--- Daw-Bank-Oscar-Lanza/src/DawBank.java
+++ Daw-Bank-Oscar-Lanza/src/DawBank.java
@@ -7,89 +7,71 @@ public class DawBank {
         Scanner sc = new Scanner(System.in);
 
 
-//        datos oscar = new datos(" ES9121000418450200051332", "Oscar-Lanza", 67.23);
-//        System.out.println("Titular: " + oscar.getTitular());
-//        System.out.println("Iban:" +  oscar.getIban());
-//        System.out.println("Saldo:" + oscar.getSaldo());
-//
-//
-//        movimientos Oscar_Movimientos = new movimientos(67, 67, "709823409A", "27 Julio 2025");
-//        System.out.println("ingreso:" + Oscar_Movimientos.getingreso());
-//        System.out.println("retirada:" + Oscar_Movimientos.getretirada());
-//        System.out.println("IDmovimiento:" + Oscar_Movimientos.getIDmovimiento());
-//        System.out.println("fecha:" + Oscar_Movimientos.getfecha());
-
-
-
-
-
-
         //Primero pondremos para que el usuario ponga sus datos
 
+        System.out.println("*********************************");
+        System.out.println("Bienvenido a tu cuenta de banco🧃");
+        System.out.println("*********************************");
 
-        System.out.println("*******************************");
-        System.out.println("Bienvenido a tu cuenta de banco");
-        System.out.println("*******************************");
+        System.out.println("Inserte el Iban");
+        String Iban = sc.nextLine();
+        System.out.println("Inserte el Titular de tu Cuenta Bancaria");     //Creamos cuenta primero ya que el usuario es tonto y puede ingresar dinero sin tener cuenta
+        String Titular = sc.nextLine();
 
-<<<<<<< HEAD:Daw-Bank-Oscar-Lanza/src/DawBank.java
-=======
-        datos MiCuenta = new datos(sc.nextLine(), sc.nextLine(), sc.nextDouble());
+        CuentaBancaria Oscar = new CuentaBancaria(Iban, Titular);  //Creamos un objeto el cual es mi cuenta bancaria
 
-        MiCuenta.getIban();
-        MiCuenta.getTitular();
-        MiCuenta.getSaldo();
 
-        movimientos MisMovimientos = new movimientos(sc.nextDouble(), sc.nextDouble(), sc.nextLine(), sc.nextLine());
 
-        MisMovimientos.getfecha();
-        MisMovimientos.getingreso();
-        MisMovimientos.getIDmovimiento();
-        MisMovimientos.getretirada();
 
->>>>>>> e5f878c32cacf9eff41473f42e10dc9e75415787:Daw-Bank-Oscar-Lanza/src/Main.java
 
         int opcion = 0;
-        while (opcion != 6){
+        while (opcion != 8){
 
-            System.out.println("Pulse 1 para crear una cuenta: ");
+            System.out.println("Pulse 1 para acceder a los datos de la cuenta ");
 
-            System.out.println("Pulse 2 para ver la informacion de la cuneta ya creada:");
+            System.out.println("Pulse 2 para ver el Iban");
 
-            System.out.println("Pulse 3 para ingresar dinero:");
+            System.out.println("Pulse 3 para mostrar el titular");
 
-            System.out.println("Pulse 4 para retirar dinero:");
+            System.out.println("Pulse 4 para mostrar el saldo");
 
-            System.out.println("Pulse 5 para mostrar los movimientos realizados en esta cuenta:");
+            System.out.println("Pulse 5 para insertar dinero en la cuenta");
 
-            System.out.println("Pulse 6 para salir del programa");
+            System.out.println("Pulse 6 para retirar Dinero");
+
+            System.out.println("Pulsa 7 para ver los movimientos realizados de la cuenta");
+
+            System.out.println("Pulse 8 para salir de este programa");
 
             opcion = sc.nextInt();
 
 
             switch (opcion) {
                 case 1:
-<<<<<<< HEAD:Daw-Bank-Oscar-Lanza/src/DawBank.java
-                    CuentaBancaria MiCuenta = new CuentaBancaria(sc.nextLine(), sc.nextLine(), sc.nextDouble());
-                    System.out.println("ingrese su IBAN");
-                    System.out.println("Ejemplo de IBAN: ES91 2100 0418 4502 0005 1332");
-                    MiCuenta.getIban();
-                    MiCuenta.setIban(sc.nextLine());
-                    System.out.println("Ingrese el nombre del titular ");
-                    MiCuenta.getTitular();
-                    MiCuenta.setTitular(sc.nextLine());
-                    System.out.println("Saldo del titular: " + MiCuenta.getSaldo());
-                    break;
-=======
 
-                break;
+                    System.out.println("Esta es tu informacion de la cuenta");
+                    Oscar.InformacionCuenta();
+                    System.out.println(Oscar.InformacionCuenta());
+
+                    break;
 
                 case 2:
+
+                    System.out.println("Este es tu Iban: ");
+                    System.out.println(Oscar.getIban()); //Ponemos Get para llamar al Iban de Cuenta Bancaria
 
                 break;
 
                 case 3:
+                    System.out.println("Este es tu Titular: ");
+                    System.out.println(Oscar.getTitular());
+
                 break;
->>>>>>> e5f878c32cacf9eff41473f42e10dc9e75415787:Daw-Bank-Oscar-Lanza/src/Main.java
+                case 4:
+                    System.out.println("Este es tu Saldo: ");
+                    System.out.println(Oscar.getSaldo());
+
+                break;
             }
 
 
